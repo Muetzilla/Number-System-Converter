0ea1319051c03d57f0ffc8a85b40a44d6fbc8a8a
Muetzilla <54369574+Muetzilla@users.noreply.github.com>
2021-04-24 14:14:21 +0200

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,16 +9,7 @@ public class Main {
     public Main(){
         interaction();
 
-        /*
-        Binary tb =  new Binary();
-        Hex hex =  new Hex();
-        String hexNum = hex.toHex(3984672);
-        String binear = tb.toBinary(1236);
 
-        System.out.println( "1236 in binär: " + binear);
-        System.out.println("3984672 in hex: " + hexNum);
-
-         */
     }
 
     private void interaction() {
@@ -27,7 +18,6 @@ public class Main {
         int inputFrom = 0;
         int inputTo = 0;
         String input = "";
-        NumbersystemFromIntToString nsIntToString = new NumbersystemFromIntToString();
         FromToNumbersystems ftns = new FromToNumbersystems();
         do{
             inputFrom = getInputFrom(scanner);
@@ -41,51 +31,6 @@ public class Main {
                 System.out.println(input + " is not a valid input for " + ftns.toString() + "! Please try again.");
 
             }
-            /*
-            switch(inputFrom){
-                    case 1:
-                        try{
-                            int dec = Integer.parseInt(input, 10);
-                            System.out.println("Your decimal input is: " + dec);
-                            validInput = true;
-                        }catch(NumberFormatException e){
-                            System.out.println(input + " is an invalid input in the decimal system! Please try again");
-                        }
-                        break;
-                    case 2:
-                        try{
-                            int hex = Integer.parseInt(input, 16);
-                            System.out.println("Your hexadecimal input is in the decimal system: " + hex);
-                            System.out.println("Your hexadecimal input is: " + input);
-                            validInput = true;
-                        }catch(NumberFormatException e){
-                            System.out.println(input + " is an invalid input in the hexadecimal system! Please try again");
-                        }
-                        break;
-                    case 3:
-                        try{
-                            new Binary(input);
-                            System.out.println("Your binary input is in the decimal system: " );
-                            System.out.println("Your binary input is: " + input);
-                            validInput = true;
-                        }catch(NumberFormatException e){
-                            System.out.println(input + " is an invalid input in the binary system! Please try again");
-                        }
-                        break;
-                    case 4:
-                        try{
-                            int oct = Integer.parseInt(input, 8);
-                            System.out.println("Your octal input is in the decimal system: " + oct);
-                            System.out.println("Yout octal input is: " + input);
-
-                            validInput = true;
-                        }catch(NumberFormatException e){
-                            System.out.println(input + " is an invalid input in the octal system! Please try again");
-
-                        }
-                        break;
-                }
-                */
 
 
         }while(continueB);
